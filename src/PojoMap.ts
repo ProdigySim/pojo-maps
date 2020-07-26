d1c58a73ce7101c8c74fc88d545fbe5ac146a0fb
Icy-Inferno <68765372+Icy-Inferno@users.noreply.github.com>
2020-07-25 18:06:59 -0700

    Implement has set remove (#6)
    * Implement has, set, and remove methods
    
    * Removed extraneous comment
    
    * Add more unit tests
    
    * Better equality testing
    
    * Documentation fixes

--- src/PojoMap.ts
+++ src/PojoMap.ts
@@ -7,6 +7,59 @@ export type PojoMap<T extends PropertyKey, U extends {}> = {
   readonly [idx in T]?: U;
 };
 
+/**
+ * Create a PojoMap from a set of key-value pairs.
+ * @param entries Key value pairs for creating the map.
+ */
+function fromEntries<T extends PropertyKey, U extends {}>(
+  entries: Readonly<Array<readonly [T, U]>>,
+): PojoMap<T, U> {
+  const acc: Partial<Record<T, U>> = {};
+  for(const [key, value] of entries) {
+    acc[key] = value;
+  }
+  return acc;
+}
+
+/**
+ * Check if a key is in a PojoMap.
+ *
+ * @param map A PojoMap
+ * @param key The key to check existence of
+ * @returns true if the key exists in the PojoMap, false otherwise.
+ */
+function has<T extends PropertyKey, U extends {}>(map: PojoMap<T,U>, key: T): boolean {
+  return typeof map[key] !== 'undefined'
+}
+
+/**
+ * Add an entry to a PojoMap, immutably.
+ *
+ * @param map An existing PojoMap
+ * @param key The entry's key
+ * @param value The entry's value
+ * @returns A new PojoMap contianing the original PojoMap and the new entry.
+ */
+function set<T extends PropertyKey, U extends {}>(map: PojoMap<T,U>, key: T, value: U): PojoMap<T,U> {
+  return {
+    ...map,
+    [key]: value,
+   };
+}
+
+/**
+ * Remove an item from a PojoMap, immutably.
+ *
+ * @param map An existing PojoMap
+ * @param key The key to remove
+ * @returns A new PojoMap contianing the original map minus the given key.
+ */
+function remove<T extends PropertyKey, U extends {}>(map: PojoMap<T,U>, key: T): PojoMap<T,U> {
+  // Extracting the the key from the PojoMap object via destructuring
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { [key]: removed, ...remaining } = map;
+  return remaining as PojoMap<T,U>;
+}
 
 /**
  * Create an empty PojoMap of the given type.
@@ -18,5 +71,9 @@ function empty<T extends PropertyKey, U extends {}>(): PojoMap<T, U> {
 }
 
 export const PojoMap = {
+  fromEntries,
+  has,
+  set,
+  remove,
   empty,
 };
--- src/__tests__/PojoMap.test.ts
+++ src/__tests__/PojoMap.test.ts
@@ -12,6 +12,83 @@ function leibnizTest<T1, T2>(id: Leibniz<T1, T2>): void {
 
 describe('PojoMap', () => {
   it('should make an empty Map', () => {
-    expect(PojoMap.empty<string, number>()).toEqual({});
+    expect(PojoMap.empty<string, number>()).toStrictEqual({});
   });
+
+  it('should check whether it is defined at a key', () => {
+    const map = PojoMap.fromEntries<string | number | symbol, number | boolean | string>([
+      ['a', 5],
+      [2, 'blue'],
+      ['orange', true],
+    ]);
+
+    expect(PojoMap.has(map, 'a')).toBe(true);
+    expect(PojoMap.has(map, 2)).toBe(true);
+    expect(PojoMap.has(map, 'orange')).toBe(true);
+  });
+
+  it('should return has() == true even when values are falsy', () => {
+    const map = PojoMap.fromEntries<string, number | boolean | string>([
+      ['a', 0],
+      ['b', false],
+      ['c', ''],
+    ]);
+    expect(PojoMap.has(map, 'a')).toBe(true);
+    expect(PojoMap.has(map, 'b')).toBe(true);
+    expect(PojoMap.has(map, 'c')).toBe(true);
+    });
+
+    it('should add to a PojoMap immutably', () => {
+      const map = PojoMap.fromEntries<string, number>([
+        ['a', 0],
+        ['b', 1],
+        ['c', 2],
+      ]);
+      const largerMap = PojoMap.set(map, 'd', 3);
+      expect(largerMap).toStrictEqual({
+        a: 0,
+        b: 1,
+        c: 2,
+        d: 3,
+      });
+    });
+
+    it('should overwrite an existing value with the same key', () => {
+      const map = PojoMap.fromEntries<string, number>([
+        ['a', 0],
+        ['b', 1],
+      ]);
+      const smallerMap = PojoMap.set(map, 'b', 5);
+      expect(smallerMap).toStrictEqual({
+        a: 0,
+        b: 5,
+      });
+    }); 
+
+    it('should remove from a PojoMap immutably', () => {
+      const map = PojoMap.fromEntries<string, number>([
+        ['a', 0],
+        ['b', 1],
+      ]);
+      const smallerMap = PojoMap.remove(map, 'b');
+      expect(smallerMap).toStrictEqual({
+        a: 0,
+      });
+      expect(PojoMap.has(smallerMap, 'b')).toStrictEqual(false);
+    });
+
+    it('should do nothing when removing a key that doesn\'t exist', () => {
+      const map = PojoMap.fromEntries<string, number>([
+        ['a', 0],
+        ['b', 1],
+      ]);
+      const newMap = PojoMap.remove(map, 'c');
+      expect(newMap).toStrictEqual({
+        a: 0,
+        b: 1,
+      });
+      expect (newMap).toStrictEqual(map);
+      expect(Object.is(newMap, map)).toBe(false);
+      expect(PojoMap.has(newMap, 'c')).toBe(false);
+    });    
 });
